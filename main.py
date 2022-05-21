92798cd7d9971102e2723d94642ce67f4387164d
Andrew-Chen-Wang <acwangpython@gmail.com>
2022-05-21 01:01:15 -0400

    Use aiohttp instead of sync requests

--- main.py
+++ main.py
@@ -12,10 +12,18 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from pathlib import Path
-from time import sleep
+"""
+Each state has their own labor law posters, so we need
+to download them and send them to our employees to abide by the law.
+"""
 
-import requests
+import asyncio
+from datetime import datetime
+from pathlib import Path
+
+import aiofiles
+from aiofiles import os
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 
 
@@ -25,7 +33,7 @@ headers = {
 }
 
 
-def _attempt_1(soup: BeautifulSoup) -> requests.Response:
+async def _attempt_1(session, soup: BeautifulSoup) -> bytes:
     pdf_link = (
         soup.find(text=lambda t: "Original poster PDF" in t)
         .parent.select_one("a")
@@ -33,71 +41,96 @@ def _attempt_1(soup: BeautifulSoup) -> requests.Response:
     )
     # This single California link sends a 403, probably because of our
     # HTTP agent: https://www.dfeh.ca.gov/wp-content/uploads/sites/32/2020/10/Workplace-Discrimination-Poster_ENG.pdf
-    try:
-        r = requests.get(pdf_link, headers=headers)
-    except requests.exceptions.SSLError:
-        raise AssertionError(f"[4] SSL error for {pdf_link}")
-    assert r.ok, f"[4] Failed for {r.url}"
-    return r
+    async with session.get(pdf_link) as r:
+        assert r.ok, f"[4a] Failed for {r.url}"
+        return await r.read()
 
 
-def _attempt_2(soup: BeautifulSoup) -> requests.Response:
+async def _attempt_2(session, soup: BeautifulSoup) -> bytes:
     link = soup.select_one("object iframe").attrs["src"]
     # Clean
     if link.startswith("/"):
         link = f"https://www.laborposters.org{link}"
     elif link.startswith("https://docs.google.com/viewer?url="):
         link = link.removeprefix("https://docs.google.com/viewer?url=").split("&")[0]
-    r = requests.get(link, headers=headers)
-    assert r.ok, f"[4] Failed for {r.url}"
-    return r
+    # Run
+    async with session.get(link) as r:
+        assert r.ok, f"[4b] Failed for {r.url}"
+        return await r.read()
 
 
-def get_pdf(state: str, r: requests.Response):
-    pdf_path = Path(f"files/{state}/{r.url.split('/')[-1]}")
-    pdf_path.parent.mkdir(parents=True, exist_ok=True)
-    if pdf_path.exists():
+async def get_pdf(session, state: str, text: str, original_url: str):
+    file = f"files/{state}/{original_url.split('/')[-1]}"
+    if await os.path.exists(file):
         return
 
-    soup = BeautifulSoup(r.content, "html.parser")
-    for x in [_attempt_1, _attempt_2]:
+    soup = BeautifulSoup(text, "html.parser")
+    errors = []
+    for i, x in enumerate([_attempt_1, _attempt_2]):
         try:
-            r = x(soup)
+            r = await x(session, soup)
+            async with aiofiles.open(file, "wb") as f:
+                await f.write(r)
             break
-        except AssertionError:
+        except BaseException as e:
+            errors.append(f"Attempt {i} for {original_url}. Error:\n{e}")
             continue
     else:
-        raise AssertionError(f"[3.5] Couldn't identify valid poster link for {r.url}")
-
-    pdf_path.write_bytes(r.content)
-
-
-def main():
-    # This could be made better with asyncio, but I don't want
-    # crash the website's server
-    r = requests.get("https://www.laborposters.org/")
-    assert r.ok, f"[1] Failed for {r.url}"
-    tbody = BeautifulSoup(r.text, "html.parser").select_one(".sf-al").parent.parent
-    links = [
-        (
-            tr.select_one("a").text,
-            tr.select("a")[0]["href"],
+        print(f"Errors for attempt:")
+        for e in errors:
+            print(e)
+        raise AssertionError(
+            f"[3.5] Couldn't identify valid poster link for {original_url}"
         )
-        for tr in tbody.select("tr")
-    ]
-    for state, link in links:
-        r = requests.get(link)
+
+
+async def get_poster(session, state, link):
+    async with session.get(link) as r:
+        assert r.ok, f"[3] Failed for {r.url}"
+        content = await r.text()
+    await get_pdf(session, state, content, link)
+
+
+async def main(session, state, link):
+    async with session.get(link) as r:
         assert r.ok, f"[2] Failed for {link}"
-        posters = BeautifulSoup(r.text, "html.parser").select(
+        posters = BeautifulSoup(await r.text(), "html.parser").select(
             ".tab-content .poster-name a"
         )
-        poster_links = [a.attrs["href"] for a in posters]
-        for poster_link in poster_links:
-            r = requests.get(poster_link)
-            assert r.ok, f"[3] Failed for {r.url}"
-            get_pdf(state, r)
-            sleep(0.1)  # to avoid rate limit
+    poster_links = [a.attrs["href"] for a in posters]
+    tasks = []
+    for poster_link in poster_links:
+        tasks.append(
+            asyncio.create_task(get_poster(session, state, poster_link))
+        )
+    await asyncio.gather(*tasks)
+
+
+async def begin():
+    async with ClientSession(headers=headers) as session:
+        async with session.get("https://www.laborposters.org/") as r:
+            assert r.ok, f"[1] Failed for {r.url}"
+            text = await r.text()
+        tbody = BeautifulSoup(text, "html.parser").select_one(".sf-al").parent.parent
+        links = [
+            (
+                tr.select_one("a").text,
+                tr.select("a")[0]["href"],
+            )
+            for tr in tbody.select("tr")
+        ]
+        for state, _ in links:
+            pdf_path = Path(f"files/{state}/")
+            pdf_path.mkdir(parents=True, exist_ok=True)
+
+        await asyncio.gather(
+            *[main(session, state, link) for state, link in links]
+        )
 
 
 if __name__ == "__main__":
-    main()
+    print(f"Starting download at {datetime.now()}")
+    Path("files").mkdir(parents=True, exist_ok=True)
+    _loop = asyncio.get_event_loop()
+    _loop.run_until_complete(begin())
+    print(f"Finished download at {datetime.now()}")
